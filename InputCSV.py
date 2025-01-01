d5db590ba6a4a66f0a6090cbe3afa3ef8c68462b
crazymannick5 <nicolasaaronwhitney1@gmail.com>
2024-12-31 23:33:29 -0500

    finished matrix intake program

--- InputCSV.py
+++ InputCSV.py
@@ -4,6 +4,8 @@ import numpy as np
 
 
 def load_csv_to_matrix(file_path):
-    df = pd.read_csv(file_path)
-    stock_names = df.columns[1:]
-    data_matrix = df.iloc[:,1:].values
+    df = pd.read_csv(file_path) #sets up the data frame
+    stock_names = df.columns[1:] #selects the stock name starting from index 1 place 2
+    data_matrix = df.iloc[:,1:].values # matrix creation, again using slicing, using integer position
+    data_matrix = np.where(data_matrix == -1, np.nan, data_matrix) #handle missing data
+    return data_matrix, stock_names
