c382b96d87c88529c4faa55b4f189ceaa67dc2c0
crazymannick5 <nicolasaaronwhitney1@gmail.com>
2024-12-31 23:22:33 -0500

    commit with csv progress

--- InputCSV.py
+++ InputCSV.py
@@ -2,5 +2,8 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv(./input.csv)
-stock_names = df.columns[1:]
+
+def load_csv_to_matrix(file_path):
+    df = pd.read_csv(file_path)
+    stock_names = df.columns[1:]
+    data_matrix = df.iloc[:,1:].values
